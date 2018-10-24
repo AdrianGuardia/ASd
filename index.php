4e077e7f35af256ed380595086589d0d9332cd31
AdrianGuardi <alumno@ciis.usil.edu.pe>
2018-10-24 10:17:52 -0500

    asd

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body >
+    <div id="bg" class="asd">
+        <div >
+            <ul >
+                <a href="">Menu</a>
+            </ul>
+        </div> 
+    </div> 
+    <div id="asd2">Pizzeria</div>  
+    <div >Pizzeria</div>  
+
+    </div>
+    
+</body>
+</html>
