5a3c3cce3b5189807a8e0fb7840901335199ac14
alumno informatica <alumno@ciis.usil.edu.pe>
2018-10-31 10:52:13 -0500

    ASD

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 </head>
 <body >
     <?php include("partes/cabecera.php") ?>
-   
+    <?php include 'partes/footer.php'?>
     
 </body>
 </html>
---
+++ partes/footer.php
@@ -0,0 +1,3 @@
+<div id="foot">
+    <p>Pizzeria la Romana Todos los Derechos registrados 2018 | Telefono 99999999</p>
+</div>
